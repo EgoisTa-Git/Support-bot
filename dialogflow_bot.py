c89063d24cb548473d9ae0f05d9df3cb64b59d5b
EgoisTa <97385729+EgoisTa-Git@users.noreply.github.com>
2023-03-23 00:02:02 +0300

    add DialogFlow learning script

--- dialogflow_bot.py
+++ dialogflow_bot.py
@@ -12,3 +12,28 @@ def detect_intent_texts(project_id, session_id, text):
         request={"session": session, "query_input": query_input}
     )
     return response.query_result.fulfillment_text
+
+
+def create_intent(project_id, display_name, training_phrases_parts,
+                  message_texts):
+    intents_client = dialogflow.IntentsClient()
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=message_texts)
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name,
+        training_phrases=training_phrases,
+        messages=[message],
+    )
+    response = intents_client.create_intent(
+        request={"parent": parent, "intent": intent}
+    )
+    return "Intent created: {}".format(response)
---
+++ learn_bot.py
@@ -0,0 +1,26 @@
+import json
+
+from environs import Env
+
+from dialogflow_bot import create_intent
+
+
+if __name__ == '__main__':
+    env = Env()
+    env.read_env()
+    project_id = env('DF_PROJECT_ID')
+
+    with open('questions.json', 'r') as file:
+        intents_json = file.read()
+    intents = json.loads(intents_json)
+
+    for display_name, intent in intents.items():
+        training_phrases_parts = intent['questions']
+        message_text = [intent['answer'], ]
+        intent_status = create_intent(
+            project_id,
+            display_name,
+            training_phrases_parts,
+            message_text,
+        )
+        print(intent_status)
