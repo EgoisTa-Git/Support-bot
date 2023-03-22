d74b36ddb877d5b262d1725194587d90a141dcb8
EgoisTa <97385729+EgoisTa-Git@users.noreply.github.com>
2023-03-22 23:08:41 +0300

    setup DialogFlow

--- main.py
+++ main.py
@@ -3,20 +3,23 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler
 from telegram.ext import Filters, CallbackContext
 
+from dialogflow_bot import detect_intent_texts
+
 
 def start(update: Update, context: CallbackContext):
-    """Send a message when the command /start is issued."""
     update.message.reply_text('Здравствуйте!')
 
 
 def help_command(update: Update, context: CallbackContext):
-    """Send a message when the command /help is issued."""
     update.message.reply_text('Help!')
 
 
-def echo(update: Update, context: CallbackContext):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+def response(update: Update, context: CallbackContext):
+    project_id = env('DF_PROJECT_ID')
+    text = update.message.text
+    session_id = update.message.chat_id
+    response_text = detect_intent_texts(project_id, session_id, text)
+    update.message.reply_text(response_text)
 
 
 if __name__ == '__main__':
@@ -25,22 +28,13 @@ if __name__ == '__main__':
     tg_bot_api_key = env('TG_BOT_APIKEY')
     # Create the Updater and pass it your bot`s token.
     updater = Updater(tg_bot_api_key)
-
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
-
-    # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
-
-    # on non command i.e. message - echo the message on Telegram
     dispatcher.add_handler(
-        MessageHandler(Filters.text & ~Filters.command, echo))
-
+        MessageHandler(Filters.text & ~Filters.command, response)
+    )
     # Start the Bot
     updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
