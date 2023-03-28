b0b28aa3f77f0c18501fe907c17e246ed6862e40
EgoisTa <97385729+EgoisTa-Git@users.noreply.github.com>
2023-03-29 00:43:25 +0300

    Add path selection for bot_learning

--- bot_learning.py
+++ bot_learning.py
@@ -1,16 +1,36 @@
+import argparse
 import json
+from pathlib import Path
 
 from environs import Env
 
 from dialogflow_api import create_intent
 
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description='Обучение нейросети бота'
+    )
+    parser.add_argument(
+        '--path',
+        type=Path,
+        default='json/questions.json',
+        help='путь к JSON-файлу с фразами для обучения нейросети',
+    )
+    args = parser.parse_args()
+    return args
+
+
 if __name__ == '__main__':
     env = Env()
     env.read_env()
     project_id = env('DF_PROJECT_ID')
+    parsed_arguments = parse_arguments()
+    if not Path.exists(parsed_arguments.path):
+        print('Указанный файл не найден!')
+        exit(0)
 
-    with open('json/questions.json', 'r') as file:
+    with open(parsed_arguments.path, 'r') as file:
         intents_json = file.read()
     intents = json.loads(intents_json)
 
